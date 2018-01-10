b32930731bbe8850f59dc1650461e5eade9ab73b
Maximilian <max.filtenborg@gmail.com>
2018-01-10 12:14:01 +0100

    Add animation script

--- traffic.py
+++ traffic.py
@@ -1,12 +1,11 @@
 #Computational science project
 #Lukas, Martijn, Lennard, Max
-#10783687,,,
+#10783687, , 11042729,
 
 import numpy as np
 
-grid = np.full((3,10),  -1, dtype=np.int32)
-step = 100
-auto = 1
+
+
 
 '''
 grid[2][3] = 3
@@ -14,16 +13,26 @@ grid[2][2] = 2
 print(np.where(grid != -1))
 '''
 
-for i in range(step):
-    coordinates = np.where(grid != -1)
-    for j in range(len(coordinates[0])):
-        if incentive:
-        
-        else:
-            #nagel-schreckenberg
-    #generate auto
-    for j in range(auto):
-        x = np.random.randint(0, 3)
-        y = np.random.randint(0, 10)
-        v = np.random.randint(1, 6)
-        grid[x][y] = v
+def simulate(animate=False):
+    grid = np.full((3,10),  -1, dtype=np.int32)
+    step = 100
+    auto = 1
+    for i in range(step):
+        coordinates = np.where(grid != -1)
+        for j in range(len(coordinates[0])):
+            if incentive:
+
+            else:
+                #nagel-schreckenberg
+        #generate auto
+        for j in range(auto):
+            x = np.random.randint(0, 3)
+            y = np.random.randint(0, 10)
+            v = np.random.randint(1, 6)
+            grid[x][y] = v
+
+        # If we want to animate the simulation, yield the grid for every step
+        if animate:
+            yield grid
+
+

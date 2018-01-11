1061caf65fe50a281878eeb151eb62d74a7ad1d1
Lukas Koedijk <lukas@koedijk.net>
2018-01-11 11:02:00 +0100

    gap and lanechange

--- traffic.py
+++ traffic.py
@@ -15,80 +15,85 @@ vback = -1
 pc = 1
 
 
-'''
-grid[2][3] = 3
-grid[2][2] = 2
-print(np.where(grid != -1))
-'''
+#nagel-scheckenberg
+def nasch(r, c, v, gap):
+    grid[r][c] = -1
+    #acceleration
+    v = min(v+1, vmax)
+    #braking
+    v = min(v, gap)
+    #randomness
+    if np.random.random() < pv:
+        v = max(v-1, 0)
+    #update
+    if c+v < collums-1:
+        grid[r][c+v] = v
 
-def calcGapo(r,rn,c,v,gridTemp):
-    gapo = 0
-    for k in range(1, vmax+1):
-        if c+k < collums-1:
-            if gridTemp[rn][c+k] == -1:
-                gapo += 1
-            else:
-                break
-        else:
-            gapo = 10
-    return gapo
 
-def calcGapoBack(r,rn,c,v, gridTemp):
-    gapoBack = 0
+
+#t is 1 dan vooruit gap en t is -1 achteruit gap
+def calcGap(r, c, gridTemp, t):
+    gap = 0
     for k in range(1, vmax+1):
-        if c-k >= 0:
-            if gridTemp[rn][c-k] == -1:
-                gapoBack += 1
+        if t == -1:
+            if c-k >= 0:
+                if gridTemp[r][c-k] == -1:
+                    gap += 1
+                else:
+                    vback = gridTemp[r][c-k]
+                    break
             else:
-                vback = gridTemp[rn][c-k]
-                break
+                gap = 10
         else:
-            gapoBack = 10
-    return gapoBack
+            if c+k < collums-1:
+                if gridTemp[r][c+k] == -1:
+                    gap += 1
+                else:
+                    break
+            else:
+                gap = 10
+    return gap
 
 def laneChange(r, c, v, gridTemp, gap, vh):
     vback = -1
     #Als de auto zich in de meest linker rijstrook bevind.
     if r == 0:
-        gapo = calcGapo(0, 1, c, v, gridTemp)
-        gapoBack = calcGapoBack(0, 1, c+gap, v, gridTemp)
+        gapo = calcGap(1, c, gridTemp, 1)
+        gapoBack = calcGap(1, c+gap, gridTemp, -1)
         grid[r][c] = -1
-        if gapo > v and gapoBack > vback:
-            if np.random.random() < pc:
-                if c+vh < collums-1:
-                    grid[1][c+vh] = vh
+        if gapo > v and gapoBack > vback and np.random.random() < pc and c+vh < collums-1:
+            grid[1][c+vh] = vh
+        else:
+            nasch(r, c, v, gap)
 
     #Als de auto zich in de meest rechter rijstrook bevind.
     elif r == rows-1:
-        gapo = calcGapo(r, r-1, c, v, gridTemp)
-        gapoBack = calcGapoBack(r, r-1, c+gap, v, gridTemp)
+        gapo = calcGap(r-1, c, gridTemp, 1)
+        gapoBack = calcGap(r-1, c+gap, gridTemp, -1)
         grid[r][c] = -1
-        if gapo > v and gapoBack > vback:
-            if np.random.random() < pc:
-                if c+vh < collums-1:
-                    grid[r-1][c+vh] = vh
+        if gapo > v and gapoBack > vback and np.random.random() < pc and c+vh < collums-1:
+            grid[r-1][c+vh] = vh
+        else:
+            nasch(r, c, v, gap)
 
     #Als de auto in een van de middelste rijstroken bevind.
     else:
-        gapoL = calcGapo(r, r-1, c, v, gridTemp)
-        gapoBackL = calcGapoBack(r, r-1, c+gap, v, gridTemp)
-        if gapoL > v and gapoBackL > vback:
-            if np.random.random() < pc:
-                if c+vh < collums-1:
-                    grid[r-1][c+vh] = vh
+        gapoL = calcGap(r-1, c, gridTemp, 1)
+        gapoBackL = calcGap(r-1, c+gap, gridTemp, -1)
+        if gapoL > v and gapoBackL > vback and np.random.random() < pc and c+vh < collums-1:
+            grid[r-1][c+vh] = vh
         else:
-            gapoR = calcGapo(r, r+1, c, v, gridTemp)
-            gapoBackR = calcGapoBack(r, r+1, c+gap, v, gridTemp)
-            if gapoR > v and gapoBackR > vback:
-                if np.random.random() < pc:
-                    if c+vh < collums-1:
-                        grid[r+1][c+vh] = vh
+            gapoR = calcGap(r+1, c, gridTemp, 1)
+            gapoBackR = calcGap(r+1, c+gap, gridTemp, -1)
+            if gapoR > v and gapoBackR > vback and np.random.random() < pc and c+vh < collums-1:
+                grid[r+1][c+vh] = vh
+            else:
+                nasch(r, c, v, gap)
 
         grid[r][c] = -1
 
 
 def simulate(animate=False):
-    print("hoi")
     for i in range(step):
         coordinates = np.where(grid != -1)
         gridTemp = grid
@@ -98,51 +103,14 @@ def simulate(animate=False):
             v = grid[r][c]
             vh = min(v+1, vmax)
 
-            gap = 0
-            for k in range(1, vmax+1):
-                if c+k < collums-1:
-                    if gridTemp[r][c+k] == -1:
-                        gap += 1
-                    else:
-                        break
-                else:
-                    gap = 10
-
+            gap = calcGap(r, c, gridTemp, 1)
             
-            '''
+            
             if vh > gap:
                 print("hoi")
                 laneChange(r, c, v, gridTemp, gap, vh)
-            '''
-            if False:
-                print("hoi")
             else:
-                #nagel-schreckenberg
-                r = coordinates[0][j]
-                c = coordinates[1][j]
-                v = grid[r][c]
-                grid[r][c] = -1
-                
-                gap = 0
-                for k in range(1, vmax+1):
-                    if c+k < collums-1:
-                        if gridTemp[r][c+k] == -1:
-                            gap += 1
-                        else:
-                            break
-                    else:
-                        gap = 10
-
-                #acceleration
-                v = min(v+1, vmax)
-                #braking
-                v = min(v, gap)
-                #randomness
-                if np.random.random() < pv:
-                    v = max(v-1, 0)
-                #update
-                if c+v < collums-1:
-                    grid[r][c+v] = v
+                nasch(r, c, v, gap)
 
         #generate auto only works for 1 car
         for l in range(auto):

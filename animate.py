9094a999cbac629b34d38be1e7404834ba804e14
Maximilian <max.filtenborg@gmail.com>
2018-01-11 15:48:24 +0100

    Some refactoring and new animations

--- animate.py
+++ animate.py
@@ -6,54 +6,57 @@ Simple animation of the highway
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from traffic import simulate
+from traffic import simulate, print_grid
+import seaborn as sb
 
 
-def update_line(num, data, line):
-    line.set_data(data[..., :num])
-    return line,
+config = {
+    'rows': 5,
+    'columns': 100,
+    'step': 100,
+}
 
+states = simulate(config, animate=True)
+grid, cars = next(states)
 
-
-grids = simulate(animate=True)
-grid = next(grids)
 fig, ax = plt.subplots()
 x = np.arange(0, 100)
 y = np.arange(0, 10)
 
 data = np.random.rand(2, 25)
 
-row_heights = [0.45, 0.5, 0.55]
-
+row_heights = np.linspace(0.3, 0.7, grid.shape[0])
 for row_height in row_heights:
     l, = ax.plot([0, 1], [row_height, row_height])
-    l, = ax.plot([0, 1], [row_height, row_height])
-    l, = ax.plot([0, 1], [row_height, row_height])
-
 
 # Scale to plot 0 - 1
-
 plt.xlim(0, 1)
 plt.ylim(0, 1)
-plt.xlabel('x')
-plt.title('test')
+plt.xlabel('Position')
+plt.title('Highway simulation')
 
-
-def plot_grid(grid):
-    lines = []
+def plot_grid(items):
+    grid, cars = items
+    markers = []
+    print_grid(grid)
     for i in range(grid.shape[0]):
         row = grid[i]
+
+        # Determines y placement
         row_height = row_heights[i]
         column_coordinates, = np.where(row != -1)
-        column_coordinates = column_coordinates / row.shape[0]
-        print(column_coordinates, np.full(column_coordinates.shape[0], row_height))
-        line, = ax.plot(column_coordinates, np.full(column_coordinates.shape[0], row_height), 'bo')
-        lines.append(line)
 
-    return (*lines),
+        # For every car we plot it as a marker with its own color
+        for column in column_coordinates:
+            car_index = row[column]
+            x_placement = column / row.shape[0]  # Scale from 0 - 1
+            marker, = ax.plot(x_placement, row_height, color=cars[car_index].color, marker='o')
+            markers.append(marker)
 
-line_ani = animation.FuncAnimation(fig, plot_grid, grids, fargs=(),
-                                   interval=500, blit=True)
+    return (*markers),  # Unpack all the new markers
+
+line_ani = animation.FuncAnimation(fig, plot_grid, states, fargs=(),
+                                   interval=250, blit=True)
 
 # To save the animation, use the command: line_ani.save('lines.mp4')
 
--- traffic.py
+++ traffic.py
@@ -7,32 +7,31 @@ import copy
 from car import Car
 
 
-rows = 5
-collums = 10
-grid = np.full((rows,collums),  -1, dtype=np.int32)
-#grid = np.array([[-1,-1,5,-1,-1,3,-1,-1,-1,-1],[-1,-1,-1,-1,2,-1,-1,-1,-1,-1],[-1,-1,2,-1,-1,3,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,3,-1,-1,-1],[-1,-1,-1,-1,2,-1,-1,-1,-1,-1]])
-step = 10
+
+# TODO: remove
+# Global vars
+speed_changes = {}
 auto = 1
 vmax = 5
 pv = 0.2
 vback = -1
 pc = 1
-speed_changes = {}
-
 
 #nagel-scheckenberg
-def nasch(r, c, v, gap, cars):
+def nasch(r, c, v, gap, grid):
     index = grid[r][c]
     grid[r][c] = -1
-    #acceleration
+
+    # acceleration
     v = min(v+1, vmax)
-    #braking
+
+    # braking
     v = min(v, gap)
-    #randomness
+    # randomness
     if np.random.random() < pv:
         v = max(v-1, 0)
-    #update
-    if c+v < collums:
+    # update
+    if c+v < grid.shape[1]:
         grid[r][c+v] = index
         speed_changes[index] = v
 
@@ -53,7 +52,7 @@ def calcGap(r, c, gridTemp, t, cars):
             else:
                 gap = 10
         else:
-            if c+k < collums:
+            if c+k < gridTemp.shape[1]:
                 if gridTemp[r][c+k] == -1:
                     gap += 1
                 else:
@@ -62,51 +61,59 @@ def calcGap(r, c, gridTemp, t, cars):
                 gap = 10
     return gap
 
-def laneChange(r, c, v, gridTemp, gap, vh, cars):
+def laneChange(r, c, v, gridTemp, gap, vh, cars, grid):
     vback = -1
     index = gridTemp[r][c]
+    columns = grid.shape[1]
+    rows = grid.shape[0]
+
     #Als de auto zich in de meest linker rijstrook bevind.
     if r == 0:
         gapo = calcGap(1, c, gridTemp, 1, cars)
         gapoBack = calcGap(1, c+gap, gridTemp, -1, cars)
         grid[r][c] = -1
-        if gapo >= v and gapoBack > vback and np.random.random() < pc and c+vh < collums:
+        if gapo >= v and gapoBack > vback and np.random.random() < pc and c+vh < columns:
             grid[1][c+vh] = index
             speed_changes[index] = vh
         else:
-            nasch(r, c, v, gap, cars)
+            nasch(r, c, v, gap, grid)
 
     #Als de auto zich in de meest rechter rijstrook bevind.
-    elif r == rows-1:
+    elif r == rows - 1:
         gapo = calcGap(r-1, c, gridTemp, 1, cars)
         gapoBack = calcGap(r-1, c+gap, gridTemp, -1, cars)
         grid[r][c] = -1
-        if gapo >= v and gapoBack > vback and np.random.random() < pc and c+vh < collums:
+        if gapo >= v and gapoBack > vback and np.random.random() < pc and c+vh < columns:
             grid[r-1][c+vh] = index
             speed_changes[index] = vh
         else:
-            nasch(r, c, v, gap, cars)
+            nasch(r, c, v, gap, grid)
 
     #Als de auto in een van de middelste rijstroken bevind.
     else:
         gapoL = calcGap(r-1, c, gridTemp, 1, cars)
         gapoBackL = calcGap(r-1, c+gap, gridTemp, -1, cars)
-        if gapoL >= v and gapoBackL > vback and np.random.random() < pc and c+vh < collums:
+        if gapoL >= v and gapoBackL > vback and np.random.random() < pc and c+vh < columns:
             grid[r-1][c+vh] = index
             speed_changes[index] = vh
         else:
             gapoR = calcGap(r+1, c, gridTemp, 1, cars)
             gapoBackR = calcGap(r+1, c+gap, gridTemp, -1, cars)
-            if gapoR >= v and gapoBackR > vback and np.random.random() < pc and c+vh < collums:
+            if gapoR >= v and gapoBackR > vback and np.random.random() < pc and c+vh < columns:
                 grid[r+1][c+vh] = index
                 speed_changes[index] = vh
             else:
-                nasch(r, c, v, gap, cars)
+                nasch(r, c, v, gap, grid)
 
         grid[r][c] = -1
 
 
-def simulate(animate=False):
+def simulate(config, animate=False):
+    rows = config['rows']
+    columns = config['columns']
+    step = config['step']
+
+    grid = np.full((rows, columns),  -1, dtype=np.int32)
     cars = {}
     for i in range(step):
         speed_changes = {}
@@ -122,50 +129,66 @@ def simulate(animate=False):
 
 
             if vh > gap:
-                laneChange(r, c, v, gridTemp, gap, vh, cars)
+                laneChange(r, c, v, gridTemp, gap, vh, cars, grid)
             else:
-                nasch(r, c, v, gap, cars)
+                nasch(r, c, v, gap, grid)
     
         # Update car speeds
         for x, y in speed_changes.items():
             cars[x].speed_changes(y)
 
-        #generate auto only works for 1 car
-        for l in range(rows):
-            if l == 4:
-                if np.random.random() < 0.7:
-                    v = np.random.randint(3,5)
-                    new_car_index = len(cars)
-                    cars[new_car_index] = Car(v, np.random.random(3), np.random.randint(2))
-                    grid[l][0] = new_car_index
-                break
+        # Generate auto only works for 1 car
+        generate_cars(cars, grid, rows)
 
-            ps = 1/float(rows+2)*(l+1)
-            if np.random.random() < ps:
-                if l == 0:
-                    v = 5
-                elif l == 1:
-                    if np.random.random() < 0.75:
-                        v = 5
-                    else:
-                        v = 4
-                elif l == 2:
-                    if np.random.random() < 0.65:
-                        v = 5
-                    else:
-                        v = 4
-                elif l == 3:
-                    if np.random.random() < 0.55:
-                        v = 5
-                    else:
-                        v = 4
-                new_car_index = len(cars)
-                cars[new_car_index] = Car(v, np.random.random(3), np.random.randint(2))
-                grid[l][0] = new_car_index
+        if i % 100 == 0:
+            remove_old_cars(cars, grid)
 
         # If we want to animate the simulation, yield the grid for every step
-        if animate:
-            yield grid, cars
+        yield grid, cars
+
+
+# Removes cars from the cars dict no longer in the grid
+def remove_old_cars(cars, grid):
+    active_cars = set(grid.flatten())
+    remove = [k for k, car in cars.items() if car not in active_cars]
+
+    for k in remove:
+        del cars[k]
+
+
+# Generates cars for every step
+def generate_cars(cars, grid, rows):
+    for i in range(rows):
+        if i == 4:
+            if np.random.random() < 0.7:
+                v = np.random.randint(3, 5)
+                new_car_index = len(cars)
+                cars[new_car_index] = Car(v, np.random.random(3), np.random.randint(2))
+                grid[i][0] = new_car_index
+            break
+
+        ps = 1 / float(rows + 2) * (i + 1)
+        if np.random.random() < ps:
+            if i == 0:
+                v = 5
+            elif i == 1:
+                if np.random.random() < 0.75:
+                    v = 5
+                else:
+                    v = 4
+            elif i == 2:
+                if np.random.random() < 0.65:
+                    v = 5
+                else:
+                    v = 4
+            elif i == 3:
+                if np.random.random() < 0.55:
+                    v = 5
+                else:
+                    v = 4
+            new_car_index = len(cars)
+            cars[new_car_index] = Car(v, np.random.random(3), np.random.randint(2))
+            grid[i][0] = new_car_index
 
 
 def print_grid(grid):
@@ -175,7 +198,13 @@ def print_grid(grid):
 
 
 if __name__ == '__main__':
-    grids = simulate(True)
+    config = {
+        'rows': 5,
+        'columns': 10,
+        'step': 10,
+    }
+
+    grids = simulate(config, animate=True)
     [print_grid(grid) for grid in grids]
 
 

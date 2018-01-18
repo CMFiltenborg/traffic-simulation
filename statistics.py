60935814cc3ee9dc2084a81211e7680d51b2228e
Lukas Koedijk <lukas@koedijk.net>
2018-01-18 12:01:11 +0100

    make graph flow/density

--- statistics.py
+++ statistics.py
@@ -1,14 +1,15 @@
 #This file wil run one of the two scrips a given ammount of times.
 #Type 0 is the original road, type 1 is theself made road.
-#use: python statistics.py type ammount
+#use: python statistics.py type steps times
 
 import sys
+import matplotlib.pyplot as plt
 from simulation import Simulation
 from RoadSection import RoadSection
 
-type, ammount = int(sys.argv[1]), int(sys.argv[2])
+type, steps, times = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 
-def original_road(ammount):
+def original_road(steps):
     spawn_r2 = [
         {0:0.5, 1:0.2, 2:0.2, 3:0.2, 4:0.2,},
         {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
@@ -24,14 +25,14 @@ def original_road(ammount):
 
     r1.set_output_mapping(outputMap)
 
-    simulation = Simulation(r2, [], ammount, 1)
+    simulation = Simulation(r2, [], steps, 1)
     result = simulation.run()
     
     [0 for r in result]
 
     return (r1, r2)
 
-def self_made_road(ammount):
+def self_made_road(steps):
     spawn_r1 = [
         {0:0.5, 1:0.2, 2:0.2, 3:0.2,},
         {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
@@ -76,7 +77,7 @@ def self_made_road(ammount):
                           0: (r7, 2),
                           })
 
-    simulation = Simulation(r1, [r2, r3, r7, r4, r5, r6, r8], ammount, 1)
+    simulation = Simulation(r1, [r2, r3, r7, r4, r5, r6, r8], steps, 1)
     result = simulation.run()
 
     [0 for r in result]
@@ -89,55 +90,65 @@ def calculate_density(roads, places):
         ammount_cars += len(roads[i].cars)
     return ammount_cars/(places*5)
 
+x, y = [], []
 #original road
 if type == 0:
-    r1, r2 = original_road(ammount)
-    
-    print("Average speed R2", r2.average_speed/ammount)
-    print("Ammount of cars finished R2", r2.finished_cars)
-    print("Flow of system", r2.finished_cars/ammount)
-    print("Density of system (cars/meter)", calculate_density((r1, r2), 100))
+    for i in range(times):
+        r1, r2 = original_road(steps)
+        flow = r2.finished_cars/steps
+        density = calculate_density((r1, r2), 100)
+        if i == 0:
+            print("Average speed R2", r2.average_speed/steps)
+            print("Ammount of cars finished R2", r2.finished_cars)
+            print("Flow of system", flow)
+            print("Density of system (cars/meter)", density)
+        x.append(density)
+        y.append(flow)
+
 #self-made road
 else:
-    r1, r2, r3, r4, r5, r6, r7, r8 = self_made_road(ammount)
+    for i in range(times):
+        r1, r2, r3, r4, r5, r6, r7, r8 = self_made_road(steps)
+        flow = (r7.finished_cars + r8.finished_cars)/steps
+        density = calculate_density((r1, r2, r3, r4, r5, r6, r7, r8), 460)
+        if i == 0:
+            average_speed_r1 = r1.average_speed/steps
+            average_speed_r2 = r2.average_speed/steps
+            average_speed_r3 = r3.average_speed/steps
+            average_speed_r4 = r4.average_speed/steps
+            average_speed_r5 = r5.average_speed/steps
+            average_speed_r6 = r6.average_speed/steps
+            average_speed_r7 = r7.average_speed/steps
+            average_speed_r8 = r8.average_speed/steps
+            
+            average_speed = (r1.average_speed/steps + r2.average_speed/steps +
+                            r3.average_speed/steps + r4.average_speed/steps +
+                            r5.average_speed/steps + r6.average_speed/steps +
+                            r7.average_speed/steps + r8.average_speed/steps) / 8
+            
+            print("Average speed R1", average_speed_r1)
+            print("Average speed R2", average_speed_r2)
+            print("Average speed R3", average_speed_r3)
+            print("Average speed R4", average_speed_r4)
+            print("Average speed R5", average_speed_r5)
+            print("Average speed R6", average_speed_r6)
+            print("Average speed R7", average_speed_r7)
+            print("Average speed R8", average_speed_r8)
 
-    average_speed_r1 = r1.average_speed/ammount
-    average_speed_r2 = r2.average_speed/ammount
-    average_speed_r3 = r3.average_speed/ammount
-    average_speed_r4 = r4.average_speed/ammount
-    average_speed_r5 = r5.average_speed/ammount
-    average_speed_r6 = r6.average_speed/ammount
-    average_speed_r7 = r7.average_speed/ammount
-    average_speed_r8 = r8.average_speed/ammount
-    
-    average_speed = (r1.average_speed/ammount + r2.average_speed/ammount +
-                    r3.average_speed/ammount + r4.average_speed/ammount +
-                    r5.average_speed/ammount + r6.average_speed/ammount +
-                    r7.average_speed/ammount + r8.average_speed/ammount) / 8
-    
-    print("Average speed R1", average_speed_r1)
-    print("Average speed R2", average_speed_r2)
-    print("Average speed R3", average_speed_r3)
-    print("Average speed R4", average_speed_r4)
-    print("Average speed R5", average_speed_r5)
-    print("Average speed R6", average_speed_r6)
-    print("Average speed R7", average_speed_r7)
-    print("Average speed R8", average_speed_r8)
-
-    print("Total average speed", average_speed)
-    print("Ammount of cars finished R7", r7.finished_cars)
-    print("Ammount of cars finished R8", r8.finished_cars)
-    print("Flow of system", (r7.finished_cars + r8.finished_cars)/ammount)
-    print("Density of system (cars/meter)", calculate_density((r1, r2, r3, r4, r5, r6, r7, r8), 460))
-
-
-
-
-
-
-
+            print("Total average speed", average_speed)
+            print("Ammount of cars finished R7", r7.finished_cars)
+            print("Ammount of cars finished R8", r8.finished_cars)
+            print("Flow of system", flow)
+            print("Density of system (cars/meter)", density)
+        x.append(density)
+        y.append(flow)
 
 
+
+plt.plot(x, y, "o")
+plt.xlabel("Density (cars/meters)")
+plt.ylabel("Flow (cars/step)")
+plt.show()
 
 
 

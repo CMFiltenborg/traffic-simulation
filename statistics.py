b6eec14dfc99f18d3782f490ed77244cc35a88e4
Lukas Koedijk <lukas@koedijk.net>
2018-01-18 12:37:29 +0100

    now using createroad in statistics

--- statistics.py
+++ statistics.py
@@ -6,83 +6,25 @@ import sys
 import matplotlib.pyplot as plt
 from simulation import Simulation
 from RoadSection import RoadSection
+from CreateRoads import CreateRoads
 
 type, steps, times = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
 
 def original_road(steps):
-    spawn_r2 = [
-        {0:0.5, 1:0.2, 2:0.2, 3:0.2, 4:0.2,},
-        {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
-    ]
-
-    r1 = RoadSection(2, 100, name='R1')
-    r2 = RoadSection(5, 100, is_end_road=True, name='R2', spawn_probabilities=spawn_r2)
-
-    outputMap = {
-        0: 3,  # Lane 1 corresponds with lane 5.
-        1: 4   # Lane 2 corresponds with lane 5.
-    }
-
-    r1.set_output_mapping(outputMap)
-
-    simulation = Simulation(r2, [], steps, 1)
+    simulation = CreateRoads.original_road(steps)
     result = simulation.run()
     
     [0 for r in result]
 
-    return (r1, r2)
+    return simulation.roads
 
 def self_made_road(steps):
-    spawn_r1 = [
-        {0:0.5, 1:0.2, 2:0.2, 3:0.2,},
-        {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
-    ]
-    spawn_r4 = [
-        {0:0.2, 1:0.2,},
-        {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
-    ]
-
-    r1 = RoadSection(4, 20, name='R1', spawn_probabilities=spawn_r1)
-    r2 = RoadSection(2, 40, name='R2')
-    r3 = RoadSection(2, 100, name='R3')
-    r4 = RoadSection(2, 40, name='R4', spawn_probabilities=spawn_r4)
-    r5 = RoadSection(1, 20, name='R5')
-    r6 = RoadSection(1, 120, name='R6')
-    r7 = RoadSection(3, 80, is_end_road=True, name='R7')
-    r8 = RoadSection(3, 40, is_end_road=True, name='R8')
-    
-    
-    r1.set_output_mapping({
-                          0: (r3, 0),
-                          1: (r3, 1),
-                          2: (r2, 0),
-                          3: (r2, 1),
-                          })
-    r2.set_output_mapping({
-                          0: (r8, 0),
-                          1: (r8, 1),
-                          })
-    r3.set_output_mapping({
-                          0: (r7, 0),
-                          1: (r7, 1),
-                          })
-    r4.set_output_mapping({
-                          0: (r5, 0),
-                          1: (r6, 0),
-                          })
-    r5.set_output_mapping({
-                          0: (r8, 2),
-                          })
-    r6.set_output_mapping({
-                          0: (r7, 2),
-                          })
-
-    simulation = Simulation(r1, [r2, r3, r7, r4, r5, r6, r8], steps, 1)
+    simulation = CreateRoads.new_design_road(steps, True)
     result = simulation.run()
 
     [0 for r in result]
 
-    return (r1, r2, r3, r4, r5, r6, r7, r8)
+    return simulation.roads
 
 def calculate_density(roads, places):
     ammount_cars = 0
@@ -94,12 +36,12 @@ x, y = [], []
 #original road
 if type == 0:
     for i in range(times):
-        r1, r2 = original_road(steps)
-        flow = r2.finished_cars/steps
-        density = calculate_density((r1, r2), 100)
+        roads = original_road(steps)
+        flow = roads[0].finished_cars/steps
+        density = calculate_density(roads, 100)
         if i == 0:
-            print("Average speed R2", r2.average_speed/steps)
-            print("Ammount of cars finished R2", r2.finished_cars)
+            print("Average speed R2", roads[0].average_speed/steps)
+            print("Ammount of cars finished R2", roads[0].finished_cars)
             print("Flow of system", flow)
             print("Density of system (cars/meter)", density)
         x.append(density)
@@ -108,9 +50,17 @@ if type == 0:
 #self-made road
 else:
     for i in range(times):
-        r1, r2, r3, r4, r5, r6, r7, r8 = self_made_road(steps)
+        roads = self_made_road(steps)
+        r1 = roads[0]
+        r2 = roads[1]
+        r3 = roads[2]
+        r4 = roads[4]
+        r5 = roads[5]
+        r6 = roads[6]
+        r7 = roads[3]
+        r8 = roads[7]
         flow = (r7.finished_cars + r8.finished_cars)/steps
-        density = calculate_density((r1, r2, r3, r4, r5, r6, r7, r8), 460)
+        density = calculate_density(roads, 460)
         if i == 0:
             average_speed_r1 = r1.average_speed/steps
             average_speed_r2 = r2.average_speed/steps

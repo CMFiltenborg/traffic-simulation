2b7accaede2d66c822ac132be494760e42df5066
Maximilian <max.filtenborg@gmail.com>
2018-01-18 11:56:50 +0100

    Add createroads class

--- animate_sim_new_v2.py
+++ animate_sim_new_v2.py
@@ -4,72 +4,21 @@ Simple animation of the highway
 """
 
 import matplotlib
-
-from simulation import Simulation
-
 matplotlib.use('TKAgg')
 
 import numpy as np
+from CreateRoads import CreateRoads
+from simulation import Simulation
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from traffic import simulate, print_grid
 from RoadSection import RoadSection
 import pylab as pl
 
-
-spawn_r1 = [
-    {0:0.5, 1:0.2, 2:0.2, 3:0.2,},
-    {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
-]
-spawn_r4 = [
-    {0:0.2, 1:0.2,},
-    {5:(0,0.2),4:(0.2,0.4),3:(0.4,1),}
-]
-
-r1 = RoadSection(4, 20, name='R1', spawn_probabilities=spawn_r1, outputColors=['red','red', 'black', 'black'])
-r2 = RoadSection(2, 40, name='R2')
-r3 = RoadSection(2, 100, name='R3')
-r4 = RoadSection(2, 40, name='R4', spawn_probabilities=spawn_r4, outputColors=['black', 'red'])
-r5 = RoadSection(1, 20, name='R5')
-r6 = RoadSection(1, 120, name='R6')
-r7 = RoadSection(3, 80, is_end_road=True, name='R7', rightLane=2)
-r8 = RoadSection(3, 40, is_end_road=True, name='R8', rightLane=2)
-
-r1.set_output_mapping({
-    0: (r3, 0),
-    1: (r3, 1),
-    2: (r2, 0),
-    3: (r2, 1),
-})
-r2.set_output_mapping({
-    0: (r8, 0),
-    1: (r8, 1),
-})
-r3.set_output_mapping({
-    0: (r7, 0),
-    1: (r7, 1),
-})
-r4.set_output_mapping({
-    0: (r5, 0),
-    1: (r6, 0),
-})
-r5.set_output_mapping({
-    0: (r8, 2),
-})
-r6.set_output_mapping({
-    0: (r7, 2),
-})
-# r7.set_output_mapping({
-#     0: (r7, 2),
-#     1: (r7, 2),
-# })
-
-
-simulation = Simulation(r1, [r2, r3, r7, r4, r5, r6, r8], 1000, 1)
+simulation = CreateRoads.new_design_road(1000, calculate_average_speed=False)
 result = simulation.run()
 
 sections = next(result)
-
 
 
 x1 = 10

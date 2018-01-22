cee2d03b9039f5e5c1358f41420850d9ccfb5390
Lukas Koedijk <lukas@koedijk.net>
2018-01-22 12:25:19 +0100

    new road added

--- CreateRoads.py
+++ CreateRoads.py
@@ -24,6 +24,57 @@ class CreateRoads:
         return simulation
 
     @staticmethod
+    def new_road(steps=100, calculate_average_speed=False):
+        spawn_r1 = [
+            {0:0.5, 1:0.2, 2:0.2, 3:0.2,},
+            {5:(0,0.2), 4:(0.2, 0.4), 3:(0.4, 1),}
+        ]
+        
+        spawn_r2 = [
+            {0:0.2,},
+            {5:(0,0.2), 4:(0.2, 0.4), 3:(0.4, 1),}
+        ]
+        
+        r1 = RoadSection(4, 10, name='R1', spawn_probabilities=spawn_r1,
+                         output_colors =['red', 'red', 'black', 'black'])
+        r2 = RoadSection(1, 10, name='R2', spawn_probabilities=spawn_r2,
+                         output_colors=['red'])
+        r5 = RoadSection(2, 90, name='R5', is_end_road=True)
+        r6 = RoadSection(2, 30, name='R6')
+        r7 = RoadSection(1, 60, name='R7', is_end_road=True)
+        r8 = RoadSection(1, 30, name='R8')
+        r9 = RoadSection(2, 60, name='R9')
+        r10 = RoadSection(3, 30, name='R10', is_end_road=True, right_lane=2)
+    
+        r1.set_output_mapping({
+            0: (r5, 0),
+            1: (r5, 1),
+            2: (r9, 0),
+            3: (r9, 1),
+        })
+    
+        r2.set_output_mapping({
+            0: (r6, 1),
+        })
+    
+        r6.set_output_mapping({
+            0: (r7, 0),
+            1: (r8, 0),
+        })
+    
+        r8.set_output_mapping({
+            0: (r10, 2),
+        })
+    
+        r9.set_output_mapping({
+            0: (r10, 0),
+            1: (r10, 1),
+        })
+        simulation = Simulation(r1, [r2, r5, r6, r7, r8, r9, r10], steps, calculate_average_speed)
+        
+        return simulation
+        
+    @staticmethod
     def new_design_road(steps=100, calculate_average_speed=False):
         spawn_r1 = [
             {0:1, 1:0.2, 2:0.2, 3:0.2,},
